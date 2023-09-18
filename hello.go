bfc4f29a892abe9684f8597c2cfe93845f175c87
Fellipe Sena <pfosena@gmail.com>
2023-09-18 19:45:47 -0300

    add first endpoint

--- hello.go
+++ hello.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main(){
-	fmt.Println("hello world!")
+	router := gin.Default()
+
+	router.GET("/hello-world", getHelloWorld)
+
+	router.Run("localhost:3000")
+}
+
+func getHelloWorld(c *gin.Context){
+	c.IndentedJSON(200, "hello world")
 }
